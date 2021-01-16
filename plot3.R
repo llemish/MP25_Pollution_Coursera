20f6a7e9799798df1da1f509c09c7aea3e1b2ae2
Levshinovskiy Mikhail <llemish@gmail.com>
2021-01-16 13:12:21 +0300

    release

--- plot3.R
+++ plot3.R
@@ -1,6 +1,6 @@
 plot3 <- function() {
         # Read file summarySCC_PM25.rds
-        NEI <- readRDS('./data/summarySCC_PM25.rds')
+        NEI <- readRDS('.//data//summarySCC_PM25.rds')
         
         # Subset Baltimore measurements by flps
         NEI_Baltimore <- subset(NEI, NEI$fips == "24510")
@@ -8,5 +8,37 @@ plot3 <- function() {
         # Use tapply for count sum by year and type
         emission <- tapply(NEI_Baltimore$Emissions, list(NEI_Baltimore$year, NEI_Baltimore$type), sum)
         
-        print(emission)
+        emission <- as.data.frame(emission)
+        
+        emission2 <- data.frame(em = rep(0, 16), typ = rep(0, 16), year = rep(0, 16))
+        
+        years <- c(1999, 2002, 2005, 2008)
+        
+        types <- names(emission)
+        
+        for (i in 1:4){
+                j <- (i-1)*4+1
+                emission2[j, 1] <- emission[1, i]
+                emission2[j+1, 1] <- emission[2, i]
+                emission2[j+2, 1] <- emission[3, i]
+                emission2[j+3, 1] <- emission[4, i]
+                
+                emission2[j, 2] <- types[i]
+                emission2[j+1, 2] <- types[i]
+                emission2[j+2, 2] <- types[i]
+                emission2[j+3, 2] <- types[i]
+                
+                emission2[j, 3] <- years[1]
+                emission2[j+1, 3] <- years[2]
+                emission2[j+2, 3] <- years[3]
+                emission2[j+3, 3] <- years[4]
+        }
+        
+        library(ggplot2)
+        
+        png(filename = "plot3.png")
+        
+        print(qplot(year, em, data = emission2, color = typ))
+        
+        dev.off()
 }
--- plot6.R
+++ plot6.R
@@ -23,30 +23,7 @@ plot6 <- function() {
     emission_b <- tapply(NEI_Vehicle_B$Emissions, NEI_Vehicle_B$year, sum)
     
     emission_la <- tapply(NEI_Vehicle_LA$Emissions, NEI_Vehicle_LA$year, sum)
-    
-    # # Subset summary set for SCC in SCC_Codes
-    # NEI_Vehicle <- NEI[NEI$SCC %in% SCC_Codes,]
-    # 
-    # # Split data frame by year
-    # NEI_by_year <- split(NEI_Vehicle, NEI_Vehicle$year)
-    # 
-    # # Get a vector of included years
-    # years <- names(NEI_by_year)
-    # 
-    # # Prepare vector that will hold total emission for every year
-    # t_emission_B = rep(0, length(years))
-    # t_emission_L = rep(0, length(years))
-    # 
-    # # Sum total emission for every year for Baltimore and Los Angeles
-    # for (i in 1:length(years)) {
-    #     # subset data frame to choose only Baltimore City (fips == "24510")
-    #     NEI_Baltimore <- subset(NEI_by_year[[i]], NEI_by_year[[i]]$fips == "24510")
-    #     t_emission_B[i] <- sum(NEI_Baltimore$Emissions, na.rm = T)
-    #     
-    #     NEI_Los_Angeles <- subset(NEI_by_year[[i]], NEI_by_year[[i]]$fips == "06037")
-    #     t_emission_L[i] <- sum(NEI_Los_Angeles$Emissions, na.rm = T)
-    # }
-    # 
+
     png(filename = "plot6.png")
     
     plot(names(emission_b), emission_b, log = "y", ylim = c(5, 5000), col = "red", ylab = "Motor Vehicle Emission (tones)")
